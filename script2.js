fd6f5008e235b71c7d22a1e8b8448cf025ccb9c3
hafid abi d <abi.daniswara@gmail.com>
2022-02-25 23:38:29 +0700

    lagi

--- script2.js
+++ script2.js
@@ -45,60 +45,41 @@ function main() {
   var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
   var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 
-  // Link the two shaders into a program
-  var program = createProgram(gl, vertexShader, fragmentShader);
+ 
+}
 
-  // look up where the vertex data needs to go.
-  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+class Aplikasi{
 
-  // Create a buffer and put three 2d clip space points in it
-  var positionBuffer = gl.createBuffer();
+  color = []
+  appProgram = null
+  gl = null
+  canvas = null
+  constructor(){
 
-  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  }
 
-  var positions = [
-    0, 0.11,
-    0.4, 0.99,
-    0.1, 0,
-  ];
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+  resizeCanvas(canva){
+    const displayWidth = canva.clientWidth;
+    const displayHeight = canva.clientHeight;
+    if (canva.width !== displayWidth || canva.height !== displayHeight) {
+        canva.width = displayWidth;
+        canva.height = displayHeight;
+    }
+    return canva
+  }
 
-  // code above this line is initialization code.
-  // code below this line is rendering code.
+  renderer(){
+    if(!this.gl || !this.appProgram) return
+    let converColor = color.map(c=>((c-255)/255)+1)
+    this.gl.useProgram(this.appProgram)
+    this.resizeCanvas(this.canvas)
+    this.gl.viewport(0,0, this.gl.canvas.width, this.gl.canvas.height)
 
-  webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-
-  // Tell WebGL how to convert from clip space to pixels
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-  // Clear the canvas
-  gl.clearColor(0, 0, 0, 0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  // Tell it to use our program (pair of shaders)
-  gl.useProgram(program);
-
-  // Turn on the attribute
-  gl.enableVertexAttribArray(positionAttributeLocation);
-
-  // Bind the position buffer.
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-  var size = 2;          // 2 components per iteration
-  var type = gl.FLOAT;   // the data is 32bit floats
-  var normalize = false; // don't normalize the data
-  var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-  var offset = 0;        // start at the beginning of the buffer
-  gl.vertexAttribPointer(
-      positionAttributeLocation, size, type, normalize, stride, offset);
-
-  // draw
-  var primitiveType = gl.TRIANGLES;
-  var offset = 0;
-  var count = 3;
-  gl.drawArrays(primitiveType, offset, count);
+    var posBuff = this.gl.createBuffer()
+    this.gl.bindBuffer(gl.ARRAY_BUFFER, posBuff)
+    this.gl.bufferData(g)
+    
+  }
 }
 
 main();
